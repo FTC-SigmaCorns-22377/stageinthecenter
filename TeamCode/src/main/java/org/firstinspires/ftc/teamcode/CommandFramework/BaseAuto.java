8d2d6bd96ba1340682915b4516475ab4d5b27013
nikhil <nvemuri4649@gmail.com>
2024-01-24 22:53:33 -0500

    uncommented out relevant stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandFramework/BaseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandFramework/BaseAuto.java
@@ -32,13 +32,13 @@ public abstract class BaseAuto extends LinearOpMode {
 
         waitForStart();
 
-//        System.out.println("randomization side");
-//        while (!isStarted()) {
-//            randomizationSide = robot.randomization.getRandomizationSide();
-//            telemetry.addData("randomization", randomizationSide.name());
-//            telemetry.update();
-//        }
-//        robot.randomization.closePortal();
+        System.out.println("randomization side");
+        while (!isStarted()) {
+            randomizationSide = robot.randomization.getRandomizationSide();
+            telemetry.addData("randomization", randomizationSide.name());
+            telemetry.update();
+        }
+        robot.randomization.closePortal();
 
         robot.getScheduler().forceCommand(setupAuto(robot.getScheduler()));
 
